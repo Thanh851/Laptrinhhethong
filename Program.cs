979447c5c578ecc264268ac8b28398ebf74c1c96
Thanh851 <thanhle.31231020750@st.ueh.edu.vn>
2026-01-13 21:43:00 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace SystemInfoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // In tieu de
+            Console.WriteLine("=== THONG TIN HE THONG ===");
+
+            // 1. In phien ban he dieu hanh
+            Console.WriteLine("Phien ban OS: " + Environment.OSVersion);
+
+            // 2. In thu muc hien tai
+            Console.WriteLine("Thu muc hien tai: " + Environment.CurrentDirectory);
+
+            // 3. In thoi gian he thong
+            Console.WriteLine("Thoi gian hien tai: " + DateTime.Now);
+
+            // Ket thuc chuong trinh
+            Console.WriteLine("==========================");
+        }
+    }
+}
